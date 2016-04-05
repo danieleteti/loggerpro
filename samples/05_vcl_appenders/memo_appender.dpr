d8e13c2c964bebee93a9f3b94ac8055e872dc19a
danieleteti <d.teti@bittime.it>
2016-04-05 15:48:10 +0200

    missed files

---
+++ samples/05_vcl_appenders/memo_appender.dpr
@@ -0,0 +1,14 @@
+program memo_appender;
+
+uses
+  Vcl.Forms,
+  MemoAppendersFormU in 'MemoAppendersFormU.pas' {MainForm};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TMainForm, MainForm);
+  Application.Run;
+end.
