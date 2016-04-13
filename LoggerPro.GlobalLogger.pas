e6d7442199279049c2f457473abe7509c7c7b638
danieleteti <d.teti@bittime.it>
2016-04-13 20:16:31 +0200

    Implemented ReleaseGlobalLogger

--- LoggerPro.GlobalLogger.pas
+++ LoggerPro.GlobalLogger.pas
@@ -1,5 +1,5 @@
 unit LoggerPro.GlobalLogger;
-{<@abstract(Contains the global logger as a thread safe singleton)
+{ <@abstract(Contains the global logger as a thread safe singleton)
   Use the global logger for fast&dirty logging, but consider to use your own
   instance of @link(ILogWriter) (created using @link(BuildLogWriter)) for all your serious logging needs.
   @author(Daniele Teti - d.teti@bittime.it)
@@ -14,6 +14,11 @@ uses
 }
 function Log: ILogWriter;
 
+{ @abstract(Use only inside DLL because dll unloading is not a safe place to shutdown threads, so call this before unload DLL)
+  Use this also in ISAPI dll. Check the @code(loggerproisapisample.dll) sample
+}
+procedure ReleaseGlobalLogger;
+
 implementation
 
 uses
@@ -22,22 +27,43 @@ uses
 var
   _Logger: ILogWriter;
   _Lock: TObject = nil;
+  _ShuttedDown: boolean = false;
 
 function Log: ILogWriter;
 begin
   if _Logger = nil then
   begin
+    if not _ShuttedDown then
+    begin
+      TMonitor.Enter(_Lock);
+      try
+        if _Logger = nil then // double check
+        begin
+          _Logger := BuildLogWriter([TLoggerProFileAppender.Create]);
+        end;
+      finally
+        TMonitor.Exit(_Lock);
+      end;
+    end;
+  end;
+  Result := _Logger;
+end;
+
+procedure ReleaseGlobalLogger;
+begin
+  if _Logger <> nil then
+  begin
     TMonitor.Enter(_Lock);
     try
-      if _Logger = nil then // double check
+      if _Logger <> nil then // double check
       begin
-        _Logger := BuildLogWriter([TLoggerProFileAppender.Create]);
+        _Logger := nil;
+        _ShuttedDown := True;
       end;
     finally
       TMonitor.Exit(_Lock);
     end;
   end;
-  Result := _Logger;
 end;
 
 initialization
