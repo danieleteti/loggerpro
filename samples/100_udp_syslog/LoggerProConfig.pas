933e1370e55d1ed0e20f99648ec7a01873c7af7e
Nurettin Onur TUĞCU <onur.tugcu@gmail.com>
2018-05-28 21:16:02 +0300

    add UDP Syslog Logger (RFC 5424) ref #11

---
+++ samples/100_udp_syslog/LoggerProConfig.pas
@@ -0,0 +1,29 @@
+unit LoggerProConfig;
+
+interface
+
+uses
+  LoggerPro, LoggerPro.UDPSyslogAppender;
+
+var
+  Log: ILogWriter;
+  Appender: TLoggerProUDPSyslogAppender;
+
+implementation
+
+initialization
+
+Appender := TLoggerProUDPSyslogAppender.Create(
+    '127.0.0.1'
+    , 5114 //UDPClientPort.Value
+    , 'COMPUTER'
+    , 'USER'
+    , 'EXE'
+    , '0.0.1'
+    , ''
+    , True
+  );
+
+Log := BuildLogWriter([Appender]);
+
+end.
